5c41fd731585261a6638920ce6bdc3863cf79cbc
Shashank Awasthi <awasthi007here@gmail.com>
2022-05-29 01:40:05 +0530

    details of signed in user and also removed the loop holes to access sign in page and all

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -50,6 +50,28 @@ module.exports.create = function(req, res){
 // sign in and create the session for user
 module.exports.createSession = function(req, res){
 
+    // find the user
+
+    // handle user found 
+    // handle password which didnt match
+    // handle session creation
+
+    // handle user not found
     
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in finding user in signing in', err);
+            return;
+        }
+        if(user){
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            return res.redirect('back');
+        }
+    })
 
 };
