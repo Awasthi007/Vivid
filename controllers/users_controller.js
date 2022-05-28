691c1c6101ea4cde7e852939ea53ca4e03b68138
Shashank Awasthi <awasthi007here@gmail.com>
2022-05-29 01:43:46 +0530

    displayed the user details and restrict the access of profile and sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,25 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res){
-    return res.render('profile', {
-        title : 'Profile Page'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(err){
+                console.log('error in finding user in profile', err);
+                return;
+            }
+            if(user){
+                return res.render('profile', {
+                    title : 'Profile page',
+                    user : user
+                })
+            }
+            return res.redirect('/users/sign-in');
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+   
 }
 
 module.exports.signUp = function (req,res){
