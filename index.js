95f1e8d481910e0baee4997c355ab5342a524e77
Shashank Awasthi <awasthi007here@gmail.com>
2022-05-28 16:27:59 +0530

    installed express-ejs-layouts and implemented the layout functionality and also extracted sheets and style from the pages to put them in the correct position at layouts

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const port = 8000;
-
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
+app.use(expressLayouts);
+// extract style and scripts from subpages
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
+
+app.use(express.static('./assets'));
 // telling use express router
 app.use('/', require('./routes/index'));
 
