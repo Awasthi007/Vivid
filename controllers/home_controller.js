a502612582ab946722c5acc71fbf378206f5e36d
Shashank Awasthi <awasthi007here@gmail.com>
2022-05-30 01:10:44 +0530

    checked authentication at posting as well as router level

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,21 @@ const Post = require('../models/posts');
 module.exports.home = function(req, res){
     //console.log(req.cookies);
 
-    Post.find({}, function(err, posts){
+    //Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log('error in finding the posts', err);
+    //         return;
+    //     }
+    //     console.log(posts);
+    //     return res.render('home', {
+    //         title: 'shashanks vivid | Home ',
+    //         posts: posts
+    //     });
+    // });
+
+
+    // populate the user so that we can access user properties
+    Post.find({}).populate('user').exec(function(err, posts){
         if(err){
             console.log('error in finding the posts', err);
             return;
